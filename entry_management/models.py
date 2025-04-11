49379534972fada82f25d26dbadabab255d15851
SavelievGleb2003 <savelievglib2003@gmail.com>
2025-04-11 09:52:53 +0300

    created a simple PlanEntrance table in the database

--- entry_management/models.py
+++ entry_management/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class PlanEntrance(models.Model):
+    entry_exit_date = models.DateTimeField()
+    entry_exit = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    department = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+    point_of_passage = models.CharField(max_length=30)
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'entry_management',
     'tailwind',
     'theme',
     'django_browser_reload',
